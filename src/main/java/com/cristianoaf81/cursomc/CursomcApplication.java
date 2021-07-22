a455462ad26067337556f99b1c1581ee2de717dd
cristiano alexandre de faria <cristianoaf81@gmail.com>
2021-07-22 16:01:56 -0300

    class18 added instantiation operation

--- src/main/java/com/cristianoaf81/cursomc/CursomcApplication.java
+++ src/main/java/com/cristianoaf81/cursomc/CursomcApplication.java
@@ -1,13 +1,34 @@
 package com.cristianoaf81.cursomc;
 
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.cristianoaf81.cursomc.domain.Categoria;
+import com.cristianoaf81.cursomc.repositories.CategoriaRepository;
+
 @SpringBootApplication
-public class CursomcApplication {
+public class CursomcApplication implements CommandLineRunner {
+	
+	@Autowired
+	private CategoriaRepository categoriaRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Categoria cat1 = new Categoria(null,"Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+		List<Categoria> items = this.categoriaRepository.findAll();
+		if (items.size() == 0) {
+			this.categoriaRepository.saveAll(Arrays.asList(cat1,cat2));
+		}
 	}
 
 }
