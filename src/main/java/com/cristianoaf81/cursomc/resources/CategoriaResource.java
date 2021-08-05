38cbfaccaef69b1ca2214e5d9b167050ea74aeb0
cristiano alexandre de faria <cristianoaf81@gmail.com>
2021-08-05 13:58:58 -0300

    class34 inserting a new Category with POST method

--- src/main/java/com/cristianoaf81/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/cristianoaf81/cursomc/resources/CategoriaResource.java
@@ -1,5 +1,6 @@
 package com.cristianoaf81.cursomc.resources;
 
+import java.net.URI;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -7,9 +8,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.cristianoaf81.cursomc.domain.Categoria;
 import com.cristianoaf81.cursomc.services.CategoriaService;
@@ -37,4 +40,16 @@ public class CategoriaResource {
 		Categoria cat = service.buscar(id);		
 		return ResponseEntity.ok().body(cat);
 	}
+
+    @RequestMapping(method=RequestMethod.POST) 
+    public ResponseEntity<Void> insert(@RequestBody Categoria obj) {
+        obj = service.insert(obj);
+        URI uri = ServletUriComponentsBuilder 
+                .fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(obj.getId())
+                .toUri();
+        return ResponseEntity.created(uri).build();      
+    }
+
 }
