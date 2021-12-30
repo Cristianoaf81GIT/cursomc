4c7c70ddc393e4b912fd43a828a3bd87d7264167
Cristiano Alexandre de Faria <cristiano.alexandre@inatel.br>
2021-12-30 08:48:40 -0300

    aula(57): criando profile de desenvolvimento

---
+++ src/main/java/com/cristianoaf81/cursomc/config/DevConfig.java
@@ -0,0 +1,37 @@
+package com.cristianoaf81.cursomc.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import com.cristianoaf81.cursomc.services.DBService;
+
+@Configuration
+@Profile("dev")
+public class DevConfig {
+
+  // @Autowired
+  // private DBService dbService;
+
+  @Autowired
+  public DevConfig(DBService dbService) {
+    this.dbService = dbService;
+  }
+
+  @Value("${spring.jpa.hibernate.ddl-auto}")
+  private String strategy;
+  private DBService dbService;
+
+  @Bean
+  public boolean instatiateDatabase() {
+    if (!"create".equals(strategy))
+      return false;
+    try {
+      this.dbService.instantiateTestDatabase();
+      return true;
+    } catch (Exception e) {
+      return false;
+    }
+  }
+}
